06365020592e4b959f721dae664dfd0bc9d2339f
Admin <hanhngao@gmail.com>
2022-10-23 14:31:04 +0700

    add

--- BACK-END/dist/src/controllers/user.controller.js
+++ BACK-END/dist/src/controllers/user.controller.js
@@ -140,10 +140,6 @@ class UserController {
     static async deleteUsers(req, res) {
         let id = req.body;
         console.log(id);
-        await user_schema_1.default.deleteOne({
-            _id: `${id}`,
-        });
-        return res.status(200).json({ message: "delete thanh cong" });
     }
     static async showFormEditCustomer(req, res) {
         let id = req.query.id;
